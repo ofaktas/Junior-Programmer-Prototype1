322f137145e883fce610ad10914c03ec69beb790
ofaktas <f.omeraktas@outlook.com>
2022-06-03 12:37:55 +0300

    +added basic movement

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -7,7 +7,10 @@ namespace UnityLearn
     public class PlayerControler : MonoBehaviour
     {
         [SerializeField] private float speed;
-         
+        [SerializeField] private float turnSpeed;
+        [SerializeField] private float HorizantalInput;
+        [SerializeField] private float ForwardInput;
+
         void Start()
         {
 
@@ -16,7 +19,13 @@ namespace UnityLearn
 
         void Update()
         {
-            if (Input.GetKey(KeyCode.W))
+            HorizantalInput = Input.GetAxis("Horizontal");
+            ForwardInput = Input.GetAxis("Vertical");
+
+            transform.Translate(Vector3.forward * Time.deltaTime * speed* ForwardInput);
+            transform.Rotate(Vector3.up * turnSpeed * HorizantalInput * Time.deltaTime);
+
+            /* if (Input.GetKey(KeyCode.W))
             {
                 transform.Translate(Vector3.forward * Time.deltaTime * speed);
             }
@@ -24,7 +33,16 @@ namespace UnityLearn
             {
                 transform.Translate(Vector3.forward* Time.deltaTime * -speed);
             }
-            
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(Vector3.up * Time.deltaTime * turnSpeed);
+            }
+            if(Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(Vector3.up * Time.deltaTime * -turnSpeed);
+            }
+            */
+
         }
     }
 
